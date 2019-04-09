9b5902715e68cc3927cb83fad22ee1bf99332915
Nitin <canvas.nv@gmail.com>
2019-04-09 08:43:56 +0530

    added pizza abstract class

---
+++ src/Pizza/Pizza.java
@@ -0,0 +1,40 @@
+package Pizza;
+
+import java.util.ArrayList;
+
+/*
+ * @author nitin
+ */
+public abstract class Pizza {
+
+    String name;
+    String dough;
+    String sauce;
+    ArrayList<String> toppings = new ArrayList<>();
+
+    void prepare() {
+        System.out.println("preparing " + name + " ...");
+        System.out.println("Tossing dough ...");
+        System.out.println("Addign sauce ...");
+        System.out.println("Adding toppings ...");
+        for (String topping : toppings) {
+            System.out.println("  " + topping);
+        }
+    }
+
+    void bake() {
+        System.out.println("Baking for 25 min");
+    }
+
+    void cut() {
+        System.out.println("Cutting in diagonal slices");
+    }
+
+    void box() {
+        System.out.println("boxing the pizza !!");
+    }
+
+    String getName() {
+        return name;
+    }
+}
